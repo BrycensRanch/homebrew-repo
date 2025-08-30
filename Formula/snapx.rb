b2bf035f645621ad46b33590f456146e15694876
okcoder1 <codershub.code@gmail.com>
2025-08-30 22:09:14 +0530

    fix: explicitly specify version
    Signed-off-by: okcoder1 <codershub.code@gmail.com>

--- Formula/snapx.rb
+++ Formula/snapx.rb
@@ -2,6 +2,7 @@ class Snapx < Formula
   desc "Screenshot tool that handles images, text, and video (fork of ShareX)"
   homepage "https://github.com/SnapXL/SnapX"
   url "https://github.com/SnapXL/SnapX/archive/homebrew.tar.gz"
+  version "0.4.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "GPL-3.0-or-later"
   head "https://github.com/SnapXL/SnapX.git", branch: "develop"
